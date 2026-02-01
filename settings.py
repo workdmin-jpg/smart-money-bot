2e57c9200d9f0d2ca971b88b3d1ca9b06b3e7d9d
workdmin-jpg <workdmin@gmail.com>
2026-02-01 05:10:55 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,46 @@
+# ===============================
+# BOT MODE
+# ===============================
+MODE = "A"   # A = Institutional | B = Active
+
+# ===============================
+# TELEGRAM
+# ===============================
+TELEGRAM_TOKEN = "8416029082:AAGrhUeUIQgml96iWQ9ma3Cu2V1lnWfgYWk"
+TELEGRAM_CHAT_ID = "8366937994"
+
+# ===============================
+# SESSION SETTINGS
+# ===============================
+ALERT_BEFORE_SESSION_MIN = 30
+
+# ===============================
+# TIMEFRAMES
+# ===============================
+HTF_A = ["30m", "1h"]
+LTF_A = ["5m", "15m"]
+
+HTF_B = ["15m", "30m"]
+LTF_B = ["5m"]
+
+# ===============================
+# LIQUIDITY FILTER
+# ===============================
+LIQUIDITY_A = ["HIGH", "MEDIUM"]
+LIQUIDITY_B = ["HIGH", "MEDIUM", "LOW"]
+
+# ===============================
+# CONTEXT APIs
+# ===============================
+CMC_API_KEY = "b07cd41441ab4317919d15ab1700839d"
+NEWS_API_KEY = "TEST_KEY"
+WHALES_API_KEY = "TEST_KEY"
+# ===============================
+# CUSTOM SYMBOLS
+# ==============================
+CUSTOM_SYMBOLS = [
+    "BTCUSDT",
+    "ETHUSDT",
+    "SOLUSDT",
+    "ARBUSDT",
+]
