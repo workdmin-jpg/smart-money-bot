d398284bd9eaebf0c6940623ffdc3580565a36bf
workdmin-jpg <workdmin@gmail.com>
2026-02-03 00:37:05 +0300

    Create manual_pairs.py

---
+++ config/manual_pairs.py
@@ -0,0 +1,11 @@
+# config/manual_pairs.py
+# ===============================
+# MANUAL PAIRS (USER CONTROLLED)
+# ===============================
+
+MANUAL_PAIRS = [
+    "BTCUSDT",
+    "ETHUSDT",
+    "SOLUSDT",
+    # أضف ما تريد
+]
